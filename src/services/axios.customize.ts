383e2753d1fc68b33862eac072bdf14f5465e94d
Nguyen Dinh Khang <dinhkhang417sc@gmail.com>
2025-04-15 09:21:35 +0700

    KhangND - get Product & User

--- src/services/axios.customize.ts
+++ src/services/axios.customize.ts
@@ -1,5 +1,28 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: "Tối quá để mai fix",
+    baseURL: "http://localhost:5000"
 });
+
+export const getUsers = async () => {
+    debugger
+    try {
+        const response = await instance.get("/api/v1/ecommerce/users");
+        console.log("API Response Data:", response.data); 
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        throw error;
+    }
+};
+
+export const getProducts = async () => {
+    try {
+        const response = await instance.get("/api/v1/ecommerce/products");
+        console.log("API Response Data:", response.data); 
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        throw error;
+    }
+};
