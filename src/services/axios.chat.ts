c06b7626ed563de872f8179dc73c4eb7397eb045
Nguyen Dinh Khang <dinhkhang417sc@gmail.com>
2025-04-20 16:58:24 +0700

    fix 5000

--- src/services/axios.chat.ts
+++ src/services/axios.chat.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://localhost:7074/api';
+const BASE_URL = 'http://localhost:5000/api';
 
 export const chatApi = {
     sendMessage: async (message: string) => {
