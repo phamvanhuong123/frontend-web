fb5010be73f99e65f1c26fb7e0e70644e4a7af88
Nguyen Dinh Khang <dinhkhang417sc@gmail.com>
2025-04-20 18:42:20 +0700

    fix loc san

--- src/layout/client/Header/index.tsx
+++ src/layout/client/Header/index.tsx
@@ -14,11 +14,10 @@ import {
   Space,
 } from "antd";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { authApi } from "../../../services/axios.auth";
 import "./header.scss";
 import { doLogoutAction } from "../../../redux/account/accountSlice";
-import { Link } from "react-router-dom";
 import ManageAccount from "../Account/ManageAccount";
 
 const Header = (props: {
@@ -68,83 +67,14 @@ const Header = (props: {
     },
   ];
 
-    return (
-        <>
-            <div className="header-container">
-                <header className="page-header">
-                    <div className="page-header__top">
-                        {/* <div
-                            className="page-header__toggle"
-                           
-                        >
-                        </div> */}
-                        <div className="page-header__logo">
-                            <span className="logo">
-                                <span onClick={() => navigate('/')}>
-                                    <img src="/logo-new.webp" alt="Logo" style={{ marginRight: 30 }} height={50}/>                               
-                                </span>
-                                <VscSearchFuzzy className="icon-search" />
-                            </span>
-                            <input
-                                className="input-search"
-                                type="text"
-                                placeholder="Hôm nay bạn muốn ăn gì?"
-                                value={props.searchTerm}
-                                style={{ width: '600px', borderRadius: '5px', height: '40px', padding: '0 10px' }}
-                                onChange={(e) => props.setSearchTerm(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <nav className="page-header__bottom">
-                        <ul id="navigation" className="navigation">
-                            <li className="navigation__item">
-                                <Popover
-                                    className="popover-carts"
-                                    placement="topRight"
-                                    rootClassName="popover-carts"
-                                    title="Sản phẩm mới thêm"
-                                    content={contentPopover}
-                                    arrow={true}
-                                >
-                                    <Badge
-                                        count={carts?.length ?? 0}
-                                        size="small"
-                                        showZero
-                                    >
-                                        <FiShoppingCart className="icon-cart" />
-                                    </Badge>
-                                </Popover>
-                            </li>
-                            <li className="navigation__item mobile">
-                                <Divider type="vertical" />
-                            </li>
-                            <li className="navigation__item mobile">
-                                {!isAuthenticated ? (                                  
-                                    <span onClick={() => navigate('/login')}>Đăng nhập</span>
-                                ) : (
-                                    <Dropdown  key={isAuthenticated} menu={{ items }} trigger={['click']}>
-                                        <Space>
-                                            <Avatar src={urlAvatar} />
-                                            {user?.fullName}
-                                        </Space>
-                                    </Dropdown>
-                                )}
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
-            </div>
-          ))
-        ) : (
-          <Empty description="Không có sản phẩm trong giỏ hàng" />
-        )}
-      </div>
-      {carts?.length > 0 && (
-        <div className="pop-cart-footer">
-          <button onClick={() => navigate("/orders")}>Xem giỏ hàng</button>
-        </div>
-      )}
+  const contentPopover = carts?.length > 0 ? (
+    <div>
+      {carts.map((item: any, index: number) => (
+        <div key={index}>{item.name}</div>
+      ))}
     </div>
+  ) : (
+    <Empty description="Không có sản phẩm trong giỏ hàng" />
   );
 
   return (
@@ -152,25 +82,29 @@ const Header = (props: {
       <div className="header-container">
         <header className="page-header">
           <div className="page-header__top">
-            <div
-              className="page-header__toggle"
-              onClick={() => setOpenDrawer(true)}
-            >
-              ☰
-            </div>
             <div className="page-header__logo">
               <span className="logo">
                 <span onClick={() => navigate("/")}>
-                  <FaReact className="rotate icon-react" />
-                  Hoàng Gia Quy Nhơn
+                  <img
+                    src="/logo-new.webp"
+                    alt="Logo"
+                    style={{ marginRight: 30 }}
+                    height={50}
+                  />
                 </span>
                 <VscSearchFuzzy className="icon-search" />
               </span>
               <input
                 className="input-search"
                 type="text"
-                placeholder="Bạn tìm gì hôm nay"
+                placeholder="Hôm nay bạn muốn ăn gì?"
                 value={props.searchTerm}
+                style={{
+                  width: "600px",
+                  borderRadius: "5px",
+                  height: "40px",
+                  padding: "0 10px",
+                }}
                 onChange={(e) => props.setSearchTerm(e.target.value)}
               />
             </div>
@@ -204,7 +138,7 @@ const Header = (props: {
                     trigger={["click"]}
                   >
                     <Space>
-                      <Avatar src={urlAvatar} />
+                      <Avatar src={user?.avatar} />
                       {user?.fullName}
                     </Space>
                   </Dropdown>
