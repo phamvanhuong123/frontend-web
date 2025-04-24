7da0fb6da4379e987ddb00cda3f1047dccc3e60b
KHANG NGUYEN <khangnguyen.010598@gmail.com>
2025-04-24 21:17:43 +0700

    Add images in cart

--- src/layout/client/Header/index.tsx
+++ src/layout/client/Header/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import { VscSearchFuzzy } from "react-icons/vsc";
 import {
